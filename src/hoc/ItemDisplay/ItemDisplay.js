c6b05752e504e07a47a7ee6e5c78d653378da048
David Williams <thedavesignal@gmail.com>
2018-10-26 14:58:12 -0700

    Implements ItemDisplay hoc

--- src/hoc/ItemDisplay/ItemDisplay.js
+++ src/hoc/ItemDisplay/ItemDisplay.js
@@ -1,1 +1,21 @@
 // A reusable item display component. Accepts name and a number as input and displays them both.
+import React, { PureComponent } from 'react';
+
+class ItemDisplay extends PureComponent{
+  render(props){
+    return (
+      <li>
+        <div>{this.props.name}</div>
+        <input
+          type="text"
+          defaultValue={this.props.price.toFixed(2)}
+          onBlur={this.props.updateHandler}
+          data-item={this.props.item}
+          style={{'color': this.props.color}}>
+        </input>
+      </li>
+    );
+  }
+}
+
+export default ItemDisplay;
